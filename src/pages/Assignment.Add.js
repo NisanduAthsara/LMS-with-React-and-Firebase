bcd7b6b2c309bb16d4b7e80cf4f66b151f2bfbed
NisanduAthsara <nisandunox@gmail.com>
2022-08-12 11:27:32 +0530

    Style new Assignment add section

--- src/pages/Assignment.Add.js
+++ src/pages/Assignment.Add.js
@@ -4,15 +4,30 @@ import AdminContext from '../Admin.Context'
 import {getAuth,onAuthStateChanged,getUserData,signOut,deleteUser} from 'firebase/auth'
 import { useCookies } from 'react-cookie';
 import { Link } from 'react-router-dom'
+import Navbar from '../components/Navbar';
+import '../styles/style.css'
 
 export default function AssignmentSectionAdd(){
     const [user,setUser] = React.useState({})
     const {getUserDetails} = React.useContext(Context)
     const {addNewAssignmentSection} = React.useContext(AdminContext)
     const [data,setData] = React.useState({})
-    const [file,setFile] = React.useState({})
+    const [file,setFile] = React.useState(null)
     const [cookies, setCookie,removeCookie] = useCookies(['token']);
     const auth = getAuth()
+    console.log(file)
+    
+    let fileName = null
+    if(file){
+        if(file.name.length > 15){
+            fileName = file.name.slice(0,15)+"....pdf"
+        }else{
+            fileName = file.name
+        }
+    }else{
+        fileName = ""
+    }
+    const labelName = fileName ? fileName : 'Select File'
 
     React.useEffect(async()=>{
         onAuthStateChanged(auth,(data)=>{
@@ -53,12 +68,39 @@ export default function AssignmentSectionAdd(){
 
     return(
         <div>
-            <form>
-                <input type="text" name='name' value={data.name} onChange={(e)=>handleChange(e)} placeholder='Name'/>
-                <input type="number" name='grade' value={data.grade} onChange={(e)=>handleChange(e)} placeholder='Grade'/>
-                <input type="file" onChange={(e)=>handleFileChange(e)}/>
+            <Navbar
+                username={user.name}
+            />
+            <div className='container'>
+                <div className='mt-3'>
+                    <div className='d-flex'>
+                        <a href='/all/assignments' className='text-decoration-none btn btn-primary custom-margin-right'>Back</a>
+                    </div>
+                </div>
+            </div>
+            <form className="container my-5">
+                <div className='row w-100'>
+                    <div className="col-sm-12 d-flex justify-content-center">
+                        <div className=''>
+                            <div className="d-flex align-items-center justify-content-center mb-3">
+                                <label for="name" className="form-label ml-5">Name: </label>
+                                <input type="text" name='name' id="name" className='form-control' value={data.name} onChange={(e)=>handleChange(e)} placeholder='Name'/>
+                            </div>
+                            <div className="d-flex align-items-center justify-content-center mb-3">
+                                <label for="grade" className="form-label ml-5">Grade: </label>
+                                <input type="number" name='grade' id="grade" className='form-control' value={data.grade} onChange={(e)=>handleChange(e)} placeholder='Grade'/>
+                            </div>
+                            <div className="text-center p-5 border border-dark rounded mb-3">
+                                <label for="file">
+                                    {labelName}
+                                    <input type="file" id="file" onChange={(e)=>handleFileChange(e)}/>
+                                </label>
+                            </div>
 
-                <button onClick={(e)=>handleSubmit(e)}>Register</button>
+                            <button onClick={(e)=>handleSubmit(e)} className='btn btn-primary'>Add</button>
+                        </div>
+                    </div>
+                </div>
             </form>
         </div>
     )
